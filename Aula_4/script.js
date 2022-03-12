4492a6e74cf7b8c4fd4126b1df2aaf75f22cd80d
lima-doug <get.douglaslima@gmail.com>
2022-03-11 21:51:20 -0300

    Desafio aula 4 Imersão Dev Alura

---
+++ Aula_4/script.js
@@ -0,0 +1,24 @@
+
+var listaFilme = [""];
+var listaName = ["Castelo no Ceu", "Chihiro", "Ponyo"];
+
+for (var i = 0; i < listaFilme.length; i++){
+
+    document.write("<img src=" + listaFilme[i] + ">");
+    document.write ("<h2>" + listaName[i] + "</h2>");
+
+}
+    
+
+function addItem(){
+    
+
+    var addImage = document.getElementById("pegarImg").value;
+    var addName = document.getElementById ("pegarNome").value;
+
+        listaFilme.push(addImage);
+        listaName.push (addName);
+
+        document.getElementById("subst-image").innerHTML += "<img src=" + addImage + ">";    
+}
+
