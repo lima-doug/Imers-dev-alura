5df75f691e84adbc586ee513e863b7bccb61f597
lima-doug <get.douglaslima@gmail.com>
2022-03-14 10:46:39 -0300

    Atualiza~

--- Aula_4/script.js
+++ Aula_4/script.js
@@ -1,11 +1,10 @@
 
 var listaFilme = [];
-var listaName = [];
+
 
 for (var i = 0; i < listaFilme.length; i++){
 
     document.write("<img src=" + listaFilme[i] + ">");
-    document.write ("<h2>" + listaName[i] + "</h2>");
 
 }
     
@@ -14,10 +13,8 @@ function addItem(){
     
 
     var addImage = document.getElementById("pegarImg").value;
-    var addName = document.getElementById ("pegarNome").value;
 
         listaFilme.push(addImage);
-        listaName.push (addName);
 
         document.getElementById("subst-image").innerHTML += "<img src=" + addImage + ">";    
 }
